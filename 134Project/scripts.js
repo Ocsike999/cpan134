cea5911931dafeafea2986f3de8e0dbe08a334ef
Ocsike999 <n01692179@humber.ca>
2024-12-06 23:37:37 -0500

    Add files via upload

---
+++ 134Project/scripts.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    if (!name || !email) {
+        alert("Please fill out all required fields.");
+        return false;
+    }
+    return true;
+}
